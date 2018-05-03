6812f73d4236d64573d12744a9052963ea24cf74
Duncan Wallace <duncwa@duncwa.com>
2018-05-03 09:26:48 -0700

    First commit and updated gitignore.

---
+++ app/src/test/java/com/duncwa/jenkinsexampleapplication/ExampleUnitTest.java
@@ -0,0 +1,27 @@
+package com.duncwa.jenkinsexampleapplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void subscription_isCorrect() throws Exception {
+
+        //WE intentionally made the string null
+        String myName = null;
+
+        assertNull(myName);
+    }
+}
+
